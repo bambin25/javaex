24bb531ca4a16e3eb108875da52603c3f21d500d
DESKTOP-8MAM7KK\wd <bambin25>
2025-02-20 16:32:05 +0900

    version2

--- src/AddDemo.java
+++ src/AddDemo.java
@@ -1,6 +1,6 @@
 public class AddDemo {
   public static void main(String[] args) {
-    int j = 0, i = 1;
+    int j = 0, i = 2;
     System.out.printf("연산 전 : j = %d, i = %d \n", j, i);
     // j = ++i;
     j = i++;
