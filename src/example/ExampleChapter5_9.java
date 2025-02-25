541cf3a384f3aa173e8698be1d9b59d3976aaec1
DESKTOP-8MAM7KK\wd <bambin25>
2025-02-25 17:47:18 +0900

    test

--- src/example/ExampleChapter5_9.java
+++ src/example/ExampleChapter5_9.java
@@ -39,14 +39,15 @@ public class ExampleChapter5_9 {
 
           System.out.println(Arrays.toString(scores));
       } else if (menuItem.equals("2")) {
-          scores = new int[numOfStudents]; // < 이게뭐지
+          scores = new int[numOfStudents]; // int[] 괄호안에 pasrInt한 numOf인 숫자 3을 넣으면서
+          // new 연산자를 통해 새로 int[] 배열을 새로 만든것을 scores배열이란 참조변수에 넣어놓는다는뜻 
           for (int i = 0; i < numOfStudents; i++) {
             System.out.printf("score[%d] : ", i);
             scores[i] = Integer.parseInt(in.nextLine());
             System.out.println(Arrays.toString(scores));
           }
       } else if (menuItem.equals("3")) {
-          for (int score : scores) {
+          for (int score : scores) {  // int socre를 새로 변수 선언함
             System.out.println(score);
           }
       } else if (menuItem.equals("4")) {
