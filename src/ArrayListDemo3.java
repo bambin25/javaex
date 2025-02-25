f46ee2023449758f38c4c8efcf4c0a55e5699219
DESKTOP-8MAM7KK\wd <bambin25>
2025-02-25 15:21:42 +0900

    ArrayList3

--- src/ArrayListDemo3.java
+++ src/ArrayListDemo3.java
@@ -10,9 +10,28 @@ public class ArrayListDemo3 {
     // 4. while 문을 사용한다 <== 점수 입력
     // 5. 입력받은 점수가 저장되어 있는 배열을 전부 읽어서 합계와 평균을 구한다.
     // 6. 입력받은 점수, 합계와 평균을 출력한다.
-    Scanner in =new Scanner(System.in);
+    Scanner in = new Scanner(System.in);
+    ArrayList<Integer> scores = new ArrayList<>();
+    int score , total = 0;
+    while (true) {
+      System.out.print("과목의 점수를 입력하세요. (입력 종료하려면 - 값 입력): ");
+      score = in.nextInt();
+      if (score < 0) {
+        break;
+      } else {
+        scores.add(score);
+      }
+    }
 
+    System.out.println("scores = " + scores);
 
+    for (int i = 0; i < scores.size(); i++) {
+      total += scores.get(i);
+    }
+
+    float avg = total / (float)scores.size();
+    System.out.println("total = " + total);
+    System.out.println("avg = " + avg);
 
   }
 }
