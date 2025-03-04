c170384a5ae38c400d80343a6ec765af0d6ee43d
DESKTOP-8MAM7KK\wd <bambin25>
2025-03-04 17:48:49 +0900

    chapter7 example

--- src/exercise/chapter7/ex3_2.java
+++ src/exercise/chapter7/ex3_2.java
@@ -3,15 +3,17 @@ package exercise.chapter7;
 public class ex3_2 {
   public static void main(String[] args) {
     Triangle t1= new Triangle(10,5);
-    Triangle t2= new Triangle(5,11);
+    Triangle t2= new Triangle(5,10);
+    int res = t1.compareTo(t2);
+
     // 결과가 넓이가 같다.
-    if (t1.compareTo(t2) == -888){
+    if (res == -888){
       System.out.println("넓이를 비교할 수 없습니다.");
     } else {
-      if (t1.compareTo(t2) > 0 ) {
-        System.out.println("t1의 넓이가 더 큽니다.");
-      } else if(t1.compareTo(t2) < 0 ) {
-        System.out.println("t2의 넓이가 더 큽니다.");
+      if (res > 0 ) {
+        System.out.println("t1"+t1+"의 넓이가 더 큽니다.");
+      } else if(res < 0 ) {
+        System.out.println("t2"+t2+"의 넓이가 더 큽니다.");
       } else {
         System.out.println("두 삼각형의 넓이가 같습니다.");
       }
@@ -44,4 +46,10 @@ class Triangle implements Comparable {
     return (height * width)/ (double)2 ;
   }
 
+  @Override
+  public String toString() {
+    return "["+
+        "넓이:"+getArea()+
+        ']';
+  }
 }
