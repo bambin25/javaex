804521136f0660928f743bbd6a2d6d39d046bfea
DESKTOP-8MAM7KK\wd <bambin25>
2025-03-14 18:55:35 +0900

    Set 인터페이스 좀더정리

--- src/collection_framework/SetDemo.java
+++ src/collection_framework/SetDemo.java
@@ -1,6 +1,7 @@
 package collection_framework;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class SetDemo {
@@ -15,6 +16,20 @@ public class SetDemo {
     set.add(5);
     set.add(20);
     System.out.println(set);
+
+    for (Integer i : set) {
+      System.out.println(i);
+    }
+
+    Iterator<Integer> iterator = set.iterator();
+    while (iterator.hasNext()) { // set의 컬렉션요소를 순회하며 다음요소가 있으면 true
+      Integer i = iterator.next();
+      if (i==10) {
+        iterator.remove(); // 값이 10인 요소를 제거
+      }
+    }
+    System.out.println(set);
+
 
 //    Set<String> s = Set.of("a", "b", "c", "d");
 //    HashSet<String> set2 = new HashSet<>(s);
